dd962b93c23ee7067f65d95a0e2dd2e78a66c9e5
Sabir <sabir@qburst.com>
2016-10-13 18:55:10 +0530

    removed margin of Column head

--- public/scripts/components/list.jsx
+++ public/scripts/components/list.jsx
@@ -5,14 +5,6 @@ constructor(props){
 	super(props);
 
 }
-componentDidUpdate(){
-	var scroller = this.refs.scroll;
-	var myScroll = new IScroll('.list', {
-    mouseWheel: true,
-    scrollbars: true
-});
-
-}
 render(){
 	
 		var val=this.props.details;
@@ -21,7 +13,7 @@ render(){
 	return(
 		<div className="list">
 	<h3>{this.props.name}</h3>
-	<div className="card-list" refs="scroll">
+	<div className="card-list">
 	{val.map((todo,key) => (<Card title={todo.title} desc={todo.desc} image={todo.image}
 						date={todo.date} version={todo.version} priority={todo.priority} key={key}/> ))}
 	<div className="drop"></div>
