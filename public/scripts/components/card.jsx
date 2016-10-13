e242fc489d76164c594a61ee6287904eaf242935
Sabir <sabir@qburst.com>
2016-10-13 17:47:30 +0530

    Added Material Design styles and wrote code for iscroll

--- public/scripts/components/card.jsx
+++ public/scripts/components/card.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 export default class extends React.Component{
 	render(){
 		return(
-			<div className="card">
-				<div>{this.props.title}</div>
-				<div>{this.props.desc}</div>
-				<div>{this.props.image}</div>
-				<div>{this.props.date}</div>
-				<div>{this.props.version}</div>
-				<div>{this.props.priority}</div>
+			<div className="mdl-card mdl-shadow--4dp card" draggable="true">
+				<div className="cards-wrap">
+					<span className="mdl-card__supporting-text card-details">{this.props.title}</span>
+					<span className="mdl-card__supporting-text card-details">{this.props.desc}</span>
+					<span className="mdl-card__supporting-text card-details">{this.props.date}</span>
+					<span className="mdl-card__supporting-text card-details">{this.props.version}</span>
+					<span className="mdl-card__supporting-text card-details">{this.props.priority}</span>
+				</div>
+				{/*<div className="image"><img className="user-img" src={this.props.image}/></div>*/}
 			</div>
 			);
 	}
--- public/scripts/components/list.jsx
+++ public/scripts/components/list.jsx
@@ -5,18 +5,27 @@ constructor(props){
 	super(props);
 
 }
+componentDidUpdate(){
+	var scroller = this.refs.scroll;
+	var myScroll = new IScroll('.list', {
+    mouseWheel: true,
+    scrollbars: true
+});
 
+}
 render(){
 	
 		var val=this.props.details;
-		
+
 
 	return(
 		<div className="list">
 	<h3>{this.props.name}</h3>
-	
-	{val.map(todo => (<Card title={todo.title} desc={todo.desc} image={todo.image}
-						date={todo.date} version={todo.version} priority={todo.priority}/> ))}
+	<div className="card-list" refs="scroll">
+	{val.map((todo,key) => (<Card title={todo.title} desc={todo.desc} image={todo.image}
+						date={todo.date} version={todo.version} priority={todo.priority} key={key}/> ))}
+	<div className="drop"></div>
+	</div>
 	</div>
 	)
 }
