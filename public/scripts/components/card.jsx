f0d6e07ec1be169b9038830a72fade1685b8720d
Sabir <sabir@qburst.com>
2016-10-14 17:51:51 +0530

    Implemented iscrolljs and HTML5 drag API

--- public/scripts/components/card.jsx
+++ public/scripts/components/card.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export default class extends React.Component{
-	render(){
+	drag(e){
+		e.dataTransfer.setData("content",e.target.id);
+	}	
+		render(){
 		return(
-			<div className="mdl-card mdl-shadow--4dp card" draggable="true">
-				<div className="cards-wrap">
+			<div className="mdl-card mdl-card--border mdl-shadow--4dp card" ref={node => this.node = node} id={this.props.title} draggable="true" onDragStart={this.drag}>
+				<div className="cards-wrap" >
 					<span className="mdl-card__supporting-text card-details">{this.props.title}</span>
 					<span className="mdl-card__supporting-text card-details">{this.props.desc}</span>
 					<span className="mdl-card__supporting-text card-details">{this.props.date}</span>
--- public/scripts/components/list.jsx
+++ public/scripts/components/list.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
-import Card from './card.jsx'
+import Card from './card.jsx';
+import IScroll from 'iscroll';
 export default class extends React.Component{
 constructor(props){
 	super(props);
-
+	this.node = undefined;
 }
+allowDrop(e){
+	e.preventDefault();
+}
+drop(e){
+	e.preventDefault();
+	var item = e.dataTransfer.getData("content");
+	e.target.appendChild(document.getElementById(item));
+}
+componentDidMount(){
+		this.iscroll = new IScroll(this.node, {
+		mouseWheel: true,
+		scrollbars: false,
+		disableMouse: true
+		})
+		// this.node.addEventListener("ondragover",function(ev){
+		// 	ev.preventDefault();
+		// })
+		// this.node.addEventListener("ondrop",function(ev){
+		// 	ev.preventDefault();
+		// 	var data = ev.dataTransfer.getData("text");
+		// 	ev.target.appendChild(document.getElementById(data));
+		// })
+}
+componentWillUnmount() {
+	this.iscroll.destroy();
+}
+
 render(){
-	
-		var val=this.props.details;
-
-
+	var val=this.props.details;
 	return(
 		<div className="list">
 	<h3>{this.props.name}</h3>
-	<div className="card-list">
+	<div className="card-list" id="drag-card" ref={node => this.node = node} onDragOver={this.allowDrop} onDrop={this.drop.bind(this)}>
+	<div className="scroll">
 	{val.map((todo,key) => (<Card title={todo.title} desc={todo.desc} image={todo.image}
 						date={todo.date} version={todo.version} priority={todo.priority} key={key}/> ))}
 	<div className="drop"></div>
+	</div>
 	</div>
 	</div>
 	)
